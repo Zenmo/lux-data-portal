dbe695cedb7c6d5ec3905de2b522a388fd1c50d9
Erik van Velzen <erik@evanv.nl>
2025-10-22 13:56:45 +0200

    Move deploy variables to repo
    Deploy variables used to be in GitHub environments. This moves them to
    the repository.
    
    Secrets are still in GitHub action secrets. In the future it's desirable
    to save them as Docker or Podman secrets.

--- github-actions/get-variables.js
+++ github-actions/get-variables.js
@@ -1,6 +1,3 @@
-
-
-
 /**
  * @typedef BuildVariables
  * @type {object}
@@ -30,15 +27,59 @@ module.exports = (context) => {
         .replaceAll(/-*$/g, '') // remove trailing dashes because it would lead to an invalid name
 
     const versionIdentifier = `${shortBranch}-${context.runNumber}`
+    const environment = getEnvironment(context)
 
-    return {
+    const baseVars = {
         ZTOR_PR_CONTAINER_APP_NAME:  `${containerAppBaseName}-${versionIdentifier}`,
         VERSION_TAG: `${versionIdentifier}-${shortCommit}`,
-        DOCKER_STACK_NAME: `zero-${getEnvironment(context)}`,
-        GITHUB_ENVIRONMENT: `swarm-${getEnvironment(context)}`,
-        // handled by wildcard certificate
-        FRONTEND_HOSTNAME: `frontend-${versionIdentifier}.zero.zenmo.com`,
-        ZTOR_HOSTNAME: `ztor-${versionIdentifier}.zero.zenmo.com`,
+        DOCKER_STACK_NAME: `zero-${environment}`,
+        GITHUB_ENVIRONMENT: `swarm-${environment}`,
+        AZURE_STORAGE_ACCOUNT_NAME: "zerostore",
+        // AZURE_STORAGE_ACCOUNT_KEY is a GitHub repository secret
+        // TODO: go to a per-environment Minio access key when we see that the feature is used
+        MINIO_ACCESS_KEY: "admin",
+        // MINIO_SECRET_KEY is a GitHub repository secret
+    }
+
+    const environmentSpecificVars = {
+        production: {
+            FRONTEND_HOSTNAME: "zero.zenmo.com",
+            ZTOR_HOSTNAME: "ztor.zero.zenmo.com",
+            DB_NAME: "zero_production",
+            // POSTGRES_PASSWORD is a GitHub environment secret
+            AZURE_STORAGE_CONTAINER: "prod",
+            MINIO_ER_EXCEL_UPLOAD_BUCKET: "energieke-regio-excel-uploads-production",
+            CORS_ALLOW_ORIGIN_PATTERN: "https://zero.zenmo.com",
+            OAUTH_CLIENT_ID: "zero-configurator-prod",
+            // OAUTH_CLIENT_SECRET is a GitHub environment secret
+        },
+        test: {
+            FRONTEND_HOSTNAME: "zero-test.zenmo.com",
+            ZTOR_HOSTNAME: "ztor-test.zero.zenmo.com",
+            DB_NAME: "zero_test",
+            // POSTGRES_PASSWORD is a GitHub environment secret
+            AZURE_STORAGE_CONTAINER: "test",
+            MINIO_ER_EXCEL_UPLOAD_BUCKET: "energieke-regio-excel-uploads-test",
+            CORS_ALLOW_ORIGIN_PATTERN: "https://zero-test.zenmo.com",
+            OAUTH_CLIENT_ID: "zero-configurator-test",
+            // OAUTH_CLIENT_SECRET is a GitHub environment secret
+        },
+        pullrequest: {
+            FRONTEND_HOSTNAME: `frontend-${versionIdentifier}.zero.zenmo.com`,
+            ZTOR_HOSTNAME: `ztor-${versionIdentifier}.zero.zenmo.com`,
+            DB_NAME: "zero_pullrequest",
+            // POSTGRES_PASSWORD is a GitHub environment secret
+            AZURE_STORAGE_CONTAINER: "dev",
+            MINIO_ER_EXCEL_UPLOAD_BUCKET: "energieke-regio-excel-uploads-pullrequest",
+            CORS_ALLOW_ORIGIN_PATTERN: "https://frontend-[\\\\w-]+.zero.zenmo.com",
+            OAUTH_CLIENT_ID: "zero-configurator-test",
+            // OAUTH_CLIENT_SECRET is a GitHub environment secret
+        },
+    }
+
+    return {
+        ...baseVars,
+        ...environmentSpecificVars[environment],
     }
 }
 

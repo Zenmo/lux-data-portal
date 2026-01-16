a77cd8b215215b351e48fdd1599c2dfa05534067
Erik van Velzen <erik@evanv.nl>
2026-01-16 09:37:24 +0100

    Update dependencies
    Update things which did not require any intervention and add some
    comments as to why some things could not be updated.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -13,9 +13,12 @@ include(
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
+            // exposed >= 0.60.0 seems to cap the constraint name for uinteger
+            // at 63 chars which causes a naming conflict
             version("exposed", "0.59.0")
-            version("kotlinx-serialization-json", "1.8.0")
-            version("kotlinx-datetime", "0.6.1")
+            version("kotlinx-serialization-json", "1.9.0")
+            // need to migrate code to kotlin.Instant to update kotlinx-datetime to 0.7.0
+            version("kotlinx-datetime", "0.6.2")
             version("kotlinx-rpc", "0.10.1")
             version("ktor", "3.3.3")
         }

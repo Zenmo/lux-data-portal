0325cac0f02c8ef5d25ff08296992e572089a17a
Erik van Velzen <erik@evanv.nl>
2026-01-16 09:37:24 +0100

    Update dependencies
    Update things which did not require any intervention and add some
    comments as to why some things could not be updated.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,6 @@
 plugins {
     // need to specify version here because this plugin in used in multiple subprojects
-    kotlin("jvm") version "2.2.21" apply false
-    kotlin("plugin.serialization") version "2.2.21" apply false
+    kotlin("jvm") version "2.3.0" apply false
+    kotlin("plugin.serialization") version "2.3.0" apply false
     id("org.jetbrains.kotlinx.rpc.plugin") version "0.10.1" apply false
-
-    // some tooling seems to prefer this format
-    //    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
-    //    id("org.jetbrains.kotlin.jvm") version "2.0.0" apply false
 }
--- ztor/build.gradle.kts
+++ ztor/build.gradle.kts
@@ -85,7 +85,7 @@ dependencies {
     implementation("io.minio:minio:8.5.17")
 
     testImplementation("io.ktor:ktor-server-test-host:${libs.versions.ktor.get()}")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:2.0.20")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:2.3.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 

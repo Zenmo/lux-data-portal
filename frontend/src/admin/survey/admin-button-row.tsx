5e56a9b6321022bcab819c66b832c80fe5ad9054
kofi-k <kofivincent026@gmail.com>
2026-08-06 18:07:46 -0700

    hide excel upload button for users without an energieke regio project (#324)
    closes #266

--- frontend/src/admin/survey/admin-button-row.tsx
+++ frontend/src/admin/survey/admin-button-row.tsx
@@ -1,9 +1,15 @@
 import {ImportExcelButton} from "./import-excel-button"
 import {NewSurveyButton} from "./new-survey-button"
+import {useProjects} from "../project/use-projects"
 
-export const AdminButtonRow = () => (
-    <div css={{display: "flex", gap: `${1/3}rem`}}>
-        <ImportExcelButton/>
-        <NewSurveyButton/>
-    </div>
-)
+export const AdminButtonRow = () => {
+    const {projects} = useProjects()
+    const hasEnergiekRegioProject = projects.some(p => p.energiekeRegioId != null)
+
+    return (
+        <div css={{display: "flex", gap: `${1/3}rem`}}>
+            {hasEnergiekRegioProject && <ImportExcelButton/>}
+            <NewSurveyButton/>
+        </div>
+    )
+}
